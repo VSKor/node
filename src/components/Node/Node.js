1818c463fbc65d5fe6f0fc5a42277ce500a64852
Vadim Korolenko <vkorolenko@provectus.local>
2016-07-14 12:32:49 +0300

    add webpack configurations and appropriate structure.

---
+++ src/components/Node/Node.js
@@ -0,0 +1,12 @@
+'use strict';
+
+class Node {
+  constructor(props){
+
+    var a = document.createElement("div");
+    a.innerHTML = "!!!!";
+    document.body.appendChild(a);
+  }
+}
+
+export default Node;
---
+++ src/main.js
@@ -0,0 +1,5 @@
+require('./scss/main.scss');
+
+import Node from './components/Node/Node';
+
+var aa = new Node;
