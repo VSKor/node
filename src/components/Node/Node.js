2a31928336ab05e5afcc7ff8d618d7c0a4525d46
Vadim Korolenko <vkorolenko@provectus.local>
2016-07-14 14:45:12 +0300

    Added: - Node creation - append method - global namespace object with some help methods.

--- src/components/Node/Node.js
+++ src/components/Node/Node.js
@@ -1,11 +1,52 @@
 'use strict';
 
 class Node {
-  constructor(props){
+  constructor(props = {}) {
+    if (!window.NodeX) {
+      window.NodeX = {
+        Nodes: [],
+        DOMNodes: [],
+        events: {},
+        subscribers: {},
+        Node: function (DOMElement) {
+          return this.Nodes[this.DOMNodes.indexOf(DOMElement)];
+        }
+      };
+    }
 
-    var a = document.createElement("div");
-    a.innerHTML = "!!!!";
-    document.body.appendChild(a);
+    this.nid = NodeX.DOMNodes.length;
+
+    NodeX.Nodes[this.nid] = this;
+    NodeX.DOMNodes[this.nid] = this.createElement.call(this, props);
+  }
+
+  createElement(props = {}) {
+    var node = document.createElement(props.element || "div");
+    delete props.element;
+
+    if (props.style) {
+      for (var prop in props.style) {
+        node.style[prop] = props.style[prop];
+      }
+      delete props.style;
+    }
+
+    if (props.attrs) {
+      for (var prop in props.attrs) {
+        node.setAttribute(prop, props.attrs[prop]);
+      }
+      delete props.attrs;
+    }
+
+    return Object.assign(node, props);
+  }
+
+  append(someNode) {
+    var nid = NodeX.Nodes.indexOf(someNode) || NodeX.DOMNodes.indexOf(someNode);
+    if(nid === -1){
+      nid = new Node(someNode).nid;
+    }
+    NodeX.DOMNodes[this.nid].appendChild(NodeX.DOMNodes[nid]);
   }
 }
 
